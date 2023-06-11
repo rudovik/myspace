2b2b45d49bc4adb9e1682d422a162a939c94addb
rudovik <flashwrite@ukr.net>
2023-06-11 19:51:57 +0300

    Changes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,10 +2,7 @@ import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-  // const users = await prisma.user.findMany()
+  const users = await prisma.user.findMany()
 
-  // console.log(users)
-
-  // return NextResponse.json(users)
-  return NextResponse.json({ message: "Hello!" })
+  return NextResponse.json(users)
 }
