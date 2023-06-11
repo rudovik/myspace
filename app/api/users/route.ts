975c29133418383428d0e657ce8f7fd1428724ed
rudovik <flashwrite@ukr.net>
2023-06-11 20:04:05 +0300

    Changes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
+export const revalidate = 0
+
 export async function GET(request: Request) {
   const users = await prisma.user.findMany()
 
