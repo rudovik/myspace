d0eaf5f7f84639cf433f14fb6d5eaf5358905bc1
rudovik <flashwrite@ukr.net>
2023-06-11 19:47:58 +0300

    Changes

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,19 +4,20 @@ import { prisma } from "@/lib/prisma"
 import { authOptions } from "../auth/[...nextauth]/route"
 
 export async function PUT(req: Request) {
-  const session = await getServerSession(authOptions)
-  const currentUserEmail = session?.user?.email!
+  // const session = await getServerSession(authOptions)
+  // const currentUserEmail = session?.user?.email!
 
-  const data = await req.json()
+  // const data = await req.json()
 
-  data.age = Number(data.age)
+  // data.age = Number(data.age)
 
-  const user = await prisma.user.update({
-    where: {
-      email: currentUserEmail,
-    },
-    data,
-  })
+  // const user = await prisma.user.update({
+  //   where: {
+  //     email: currentUserEmail,
+  //   },
+  //   data,
+  // })
 
-  return NextResponse.json(user)
+  // return NextResponse.json(user)
+  return NextResponse.json({ message: "Hello" })
 }
