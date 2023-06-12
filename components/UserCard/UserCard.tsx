425f1cb5773ffd7b4f987bac7234152853572a44
rudovik <flashwrite@ukr.net>
2023-06-12 11:58:38 +0300

    Changes

--- components/UserCard/UserCard.tsx
+++ components/UserCard/UserCard.tsx
@@ -8,6 +8,8 @@ interface Props {
   image: string | null
 }
 
+export const revalidate = 0
+
 export default function UserCard({ id, name, age, image }: Props) {
   return (
     <div className={styles.card}>
