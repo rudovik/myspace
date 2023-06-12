172a33ba65083ce5a2df87bea8683181d17a2cc6
rudovik <flashwrite@ukr.net>
2023-06-12 17:29:47 +0300

    Changes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,15 +2,20 @@ import { prisma } from "@/lib/prisma"
 import styles from "./page.module.css"
 import UserCard from "@/components/UserCard/UserCard"
 
-async function getUsers() {
-  const users = await prisma.user.findMany()
-  return users
+async function getUsers(): Promise<User[]> {
+  const res = await fetch("/api/users", { next: { revalidate: 0 } })
+  return await res.json()
 }
 
-export const revalidate = 0
+interface User {
+  id: string
+  name: string
+  age: number
+  image: string
+}
 
 export default async function Users() {
-  const users = await getUsers()
+  const users: User[] = await getUsers()
 
   return (
     <main className={styles.grid}>
