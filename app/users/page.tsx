aca754f29ae2b1c3250488d01672eaf7a9ec7b89
rudovik <flashwrite@ukr.net>
2023-06-12 22:19:11 +0300

    Changes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma"
 import styles from "./page.module.css"
 import UserCard from "@/components/UserCard/UserCard"
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 async function getUsers() {
   const users = await prisma.user.findMany()
@@ -10,7 +12,11 @@ async function getUsers() {
 export const revalidate = 0
 
 export default async function Users() {
+  const router = useRouter()
   const users = await getUsers()
+  useEffect(() => {
+    router.refresh()
+  }, [])
 
   return (
     <main className={styles.grid}>
